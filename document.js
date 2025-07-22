11235513572aa328771db7cfd8e2a8f9b0d851b8
KridayMinecraft-20 <apareek08@gmail.com>
2025-07-22 18:49:55 +0530

    Add files via upload

---
+++ document.js
@@ -0,0 +1,9 @@
+var div = document.createElement(`div`)
+div.id = "paragraph"
+div.className = "alert"
+
+div.innerHTML = "<p>Congrats! You have created a div. <p>"
+document.body.append(div)
+
+let heading = document.getElementById("heading")
+heading.innerHTML = '&#128512'
