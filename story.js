0cee17c30eae0753ff70680ba97c789dd6e8be8b
KridayMinecraft-20 <apareek08@gmail.com>
2025-07-24 18:57:49 +0530

    Add files via upload
    All 3 are meant to be together #CodingRules #FollowMe #HorrorStoryVibes #onButtonClick

---
+++ story.js
@@ -0,0 +1,15 @@
+function typeAndScream() {
+
+    var personName = document.getElementById ("name").value;
+    alert("I told you not to do that " + personName);
+}
+
+function goDark() {
+
+    document.body.className = "dark";
+}
+
+function finishTheStory() {
+    document.getElementById("story").innerHTML = "So I carefully searched the damp dungeon, There you are!"
+
+}
