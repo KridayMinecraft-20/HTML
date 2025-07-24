86686e5396d87dafc904803dac8e796657490d6b
KridayMinecraft-20 <apareek08@gmail.com>
2025-07-24 16:36:45 +0530

    Add files via upload

---
+++ multiplication.js
@@ -0,0 +1,14 @@
+function productOfTwoNumbers() {
+
+   var x,y,z
+   x = 92;
+   y = 23;
+   z = y * x;
+
+   
+   return z;
+
+}
+
+var product = 2116;
+console.log(`The product of 92 and 23 is ` + product)
